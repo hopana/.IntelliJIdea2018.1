b8a84422a0f6b143842127a07f29199ccc6d5c25
hopana <hopana@163.com>
2018-04-10 18:30:18 +0800

    初始提交

---
+++ config/consoles/db/d95b2282-7a3e-4449-a284-72e1a59fde5d/console_1.sql
@@ -0,0 +1,10 @@
+
+        SELECT
+        c.channel_id_name parent_ch_name,
+        
+        FROM cms_audit_channel t
+        LEFT JOIN cms_channel c
+        ON t.parent_ch_id = c.channel_id
+        
+        
+    
---
+++ config/fileTemplates/includes/ActionScript File Header.as
@@ -0,0 +1,6 @@
+/**
+ * TODO 注释
+ *
+ * @author hupan
+ * @since ${YEAR}-${MONTH}-${DAY} ${TIME}
+ */
