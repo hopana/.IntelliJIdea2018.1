b35bccf0f6b3a66fa90145ba6fbc8ae67fd8d219
hopana <hopana@163.com>
2018-04-10 18:47:45 +0800

    提交system文件夹

---
+++ system/tmp/intellij-git-askpass.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:/Program Files/ideaIU-2018.1.win/jre64/bin/java" -cp "C:/Program Files/ideaIU-2018.1.win/plugins/git4idea/lib/git4idea-rt.jar;C:/Program Files/ideaIU-2018.1.win/lib/xmlrpc-2.0.1.jar;C:/Program Files/ideaIU-2018.1.win/lib/commons-codec-1.9.jar;C:/Program Files/ideaIU-2018.1.win/lib/util.jar" org.jetbrains.git4idea.http.GitAskPassApp %*
---
+++ system/tmp/intellij-git-ssh.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:/Program Files/ideaIU-2018.1.win/jre64/bin/java" -cp "C:/Program Files/ideaIU-2018.1.win/plugins/git4idea/lib/git4idea-rt.jar;C:/Program Files/ideaIU-2018.1.win/lib/xmlrpc-2.0.1.jar;C:/Program Files/ideaIU-2018.1.win/lib/commons-codec-1.9.jar;C:/Program Files/ideaIU-2018.1.win/lib/util.jar;C:/Program Files/ideaIU-2018.1.win/lib/trilead-ssh2-build-217-jenkins-11.jar;C:/Program Files/ideaIU-2018.1.win/lib/eddsa-0.2.0.jar;C:/Program Files/ideaIU-2018.1.win/lib/jbcrypt-1.0.0.jar" org.jetbrains.git4idea.ssh.SSHMain %*
